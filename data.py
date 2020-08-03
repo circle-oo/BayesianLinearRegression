466b08a502f62770cc6dcd35f4e9f1a5e547e5dd
Jiun Bae <baejiun@navercorp.com>
2020-08-03 16:45:43 +0900

    Refactoring

--- data.py
+++ data.py
@@ -1,19 +1,23 @@
+from typing import Tuple
+
 import numpy as np
 
 
 class Data:
-    def __init__(self):
-        self.x = []
-        self.t = []
-    
-    def addData(self, n):
-        for i in range(n):
-            self.x.append(np.random.rand(1))
-            self.t.append(np.sin(2*np.pi*self.x[-1]) + np.random.normal(0, 0.1))
-    
-    def getData(self):
-        return self.x
-    
-    def getTarget(self):
-        return self.t
+    def __init__(self, mean: float = .0, std: float = .01):
+        self.mean = mean
+        self.std = std
 
+        self.x = np.empty((0, 1))
+        self.y = np.empty((0, 1))
+    
+    def add(self, n: int):
+        x = np.random.rand(n, 1)
+        y = np.sin(2 * np.pi * x) + np.random.normal(self.mean, self.std, size=(n, 1))
+
+        self.x = np.concatenate((self.x, x))
+        self.y = np.concatenate((self.y, y))
+    
+    def get(self) \
+            -> Tuple[np.ndarray, np.ndarray]:
+        return self.x, self.y
